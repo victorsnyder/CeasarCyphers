e626cdb6a48bb77600d47f1137d7ac3cf4f5e238
victorsnyder <42549605+victorsnyder@users.noreply.github.com>
2018-09-08 22:17:16 -0400

    Added character verification code.

--- TestProject/src/Main.java
+++ TestProject/src/Main.java
@@ -3,9 +3,6 @@ import java.awt.Rectangle;
 public class Main {
 	public static void main(String[] args) {
 		
-		displayGreeting();
-	}
-	private static void displayGreeting() {
 		String salute = "Hey buddy.";
 		// var assignment
 		salute = salute.replace("buddy","loser");
@@ -17,6 +14,12 @@ public class Main {
 		Rectangle box = new Rectangle(5, 5, 15, 50);
 		box.translate(-3,10);
 		System.out.println(box.getX());
+		char ch = '5';
+		if (Character.isLetter(ch))
+			{System.out.println("this be a character");
+			}	
+		else
+			System.out.println("this not be a character");
+	}
 		
 	}
-}
