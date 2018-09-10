52aff1de199f7af07ee2644ad1cf03b1f1d5345a
victorsnyder <42549605+victorsnyder@users.noreply.github.com>
2018-09-09 23:37:48 -0400

    made an ellipse

--- TestProject/src/Rectum.java
+++ TestProject/src/Rectum.java
@@ -3,6 +3,7 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import java.awt.geom.Ellipse2D;
 
 public class Rectum extends JComponent
 {
@@ -16,20 +17,27 @@ public class Rectum extends JComponent
 			g2.draw(box);
 			box.translate(20, 40);
 			g2.draw(box);
-			
+			//other way to draw le box
+			g2.drawRect(20, 20, 20, 20);
+			//imported and drew code for circle.
+			Ellipse2D.Double ellipse = new Ellipse2D.Double(20, 30, 500, 200);
+			g2.draw(ellipse);
 	}
+	
 		
 	public static void main (String[] args) 
 	{
 		JFrame frame = new JFrame();
 		
-		frame.setSize(200, 200);
+		frame.setSize(600, 500);
 		
 		Rectum component = new Rectum();
 		
 		frame.add(component);
 		
 		frame.setVisible(true);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 				
 			
